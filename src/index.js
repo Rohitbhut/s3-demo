0e60844f03c22726c90f21f961b8fa4ccba72cce
Rohitbhut <bhutrohit@gmail.com>
2023-02-07 00:46:31 +0530

    small update for index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const port=process.env.PORT || 3000;
 app.use(express.json());
 
 app.use('/home',(req,res)=>{
-    res.json({msg:'Hello I am Rohit Patel'});
+    res.json({msg:'Hello I am Rohit Bhut'});
 });
 
 //User Routing App
